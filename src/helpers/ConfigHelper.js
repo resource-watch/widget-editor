39a2fd6ed1b57f98884d9f14e2334d6b04b58ae7
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2017-11-24 10:57:30 +0000

    Improve the documentation of ConfigHelper and add the userEmail param

--- src/helpers/ConfigHelper.js
+++ src/helpers/ConfigHelper.js
@@ -1,5 +1,13 @@
 let config = null;
 
+/**
+ * @typedef {{ url: string, env: string, applications: string, userToken?: string, userEmail?: string}} WidgetEditorConfig
+ */
+
+/**
+ * Get the configuration of the widget editor
+ * @return {WidgetEditorConfig}
+ */
 export function getConfig() {
   if (!config) {
     throw new Error('You must provide a configuration for widget-editor.'); // TODO: library name
@@ -8,6 +16,10 @@ export function getConfig() {
   }
 };
 
+/**
+ * Set the configuration of the widget editor
+ * @param {WidgetEditorConfig} params Configuration of the widget editor
+ */
 export function setConfig(params) {
   if (config) return;
 
@@ -15,10 +27,15 @@ export function setConfig(params) {
     throw new Error('The configuration of widget-editor must provide the env, applications and url attributes.'); // TODO: library name
   }
 
+  if (params && ((params.userToken && !params.userEmail) || !params.userToken && params.userEmail)) {
+    throw new Error('If the user is logged in, specify their token *and* email in the configuration of the widget-editor.'); // TODO: library name
+  }
+
   config = {
     url: params.url,
     env: params.env,
     applications: params.applications,
-    userToken: params.userToken || null
+    userToken: params.userToken || null,
+    userEmail: params.userEmail || null
   };
 };
