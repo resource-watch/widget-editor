resource-watch/widget-editor

commit 03c70e7782b58f7949b413c9e161806a1e096a05

add title to props so parent can access (#4)
Let the title of the widget being controlled from the outside

src/components/WidgetEditor.js
@@ -217,6 +217,13 @@ class WidgetEditor extends React.Component {
           : []
       });
     }
+
+    // If the title is controlled from the outside and
+    // its value is changed, then we update the store
+    if (nextProps.widgetTitle !== undefined
+      && nextProps.widgetTitle !== this.props.widgetEditor.title) {
+      this.props.setTitle(nextProps.widgetTitle);
+    }
   }
 
   componentDidUpdate(previousProps, previousState) {
@@ -437,11 +444,11 @@ class WidgetEditor extends React.Component {
     } = this.state;
 
     const { widgetEditor, datasetId, selectedVisualizationType } = this.props;
-    const { chartType, layer, zoom, latLng } = widgetEditor;
+    const { chartType, layer, zoom, latLng, title } = widgetEditor;
 
     let chartTitle = (
       <div className="chart-title">
-        <span>{widgetEditor.title}</span>
+        <span>{title}</span>
       </div>
     );
     if (this.props.titleMode === 'always'
@@ -450,7 +457,7 @@ class WidgetEditor extends React.Component {
         <div className="chart-title">
           <AutosizeInput
             name="widget-title"
-            value={widgetEditor.title || ''}
+            value={title || ''}
             placeholder="Widget title"
             onChange={this.handleTitleChange}
           />
@@ -958,6 +965,9 @@ class WidgetEditor extends React.Component {
   handleTitleChange(event) {
     const title = event.target.value;
     this.props.setTitle(title);
+    if (this.props.onChangeWidgetTitle) {
+      this.props.onChangeWidgetTitle(title);
+    }
   }
 
   /**
@@ -1199,6 +1209,10 @@ WidgetEditor.propTypes = {
    */
   widgetId: PropTypes.string,
   /**
+   * Widget title (if the editor is used to edit an existing  widget)
+   */
+  widgetTitle: PropTypes.string,
+  /**
    * List of visualizations that are available to the widget editor
    */
   availableVisualizations: PropTypes.arrayOf(
@@ -1247,6 +1261,11 @@ WidgetEditor.propTypes = {
    * to get the widget config
    */
   provideWidgetConfig: PropTypes.func,
+  /**
+   * Callback executed when the value of the title is updated
+   * The callback gets passed the new value
+   */
+  onChangeWidgetTitle: PropTypes.func,
   // Store
   band: PropTypes.object,
   widgetEditor: PropTypes.object.isRequired,

src/components/modal/SaveWidgetModal.js
@@ -54,6 +54,19 @@ class SaveWidgetModal extends React.Component {
     };
   }
 
+  /**
+   * Event handler executed when the user changes the
+   * title of the widget
+   * @param {string} event
+   */
+  @Autobind
+  onChangeTitle(title) {
+    this.props.setTitle(title);
+    if (this.props.onChangeWidgetTitle) {
+      this.props.onChangeWidgetTitle(title);
+    }
+  }
+
 
   @Autobind
   async onSubmit(event) {
@@ -139,7 +152,7 @@ class SaveWidgetModal extends React.Component {
             <fieldset className="c-we-field-container">
               <Field
                 ref={(c) => { if (c) FORM_ELEMENTS.elements.title = c; }}
-                onChange={value => this.props.setTitle(value)}
+                onChange={this.onChangeTitle}
                 validations={['required']}
                 properties={{
                   title: 'title',
@@ -229,6 +242,11 @@ SaveWidgetModal.propTypes = {
    * button to check their widgets
    */
   onClickCheckWidgets: PropTypes.func,
+  /**
+   * Callback executed when the value of the title is updated
+   * The callback gets passed the new value
+   */
+  onChangeWidgetTitle: PropTypes.func,
 
   // Store
   widgetEditor: PropTypes.object.isRequired,

test.js
@@ -48,7 +48,8 @@ class App extends React.Component {
     super(props);
     this.state = {
       datasetId: '0b9f0100-ce5b-430f-ad8f-3363efa05481',
-      widgetId: undefined
+      widgetId: undefined,
+      widgetTitle: ''
     };
   }
 
@@ -65,7 +66,8 @@ class App extends React.Component {
         childrenProps: {
           datasetId: this.state.datasetId,
           getWidgetConfig: this.getWidgetConfig,
-          onClickCheckWidgets: () => alert('Check my widgets')
+          onClickCheckWidgets: () => alert('Check my widgets'),
+          onChangeWidgetTitle: title => this.setState({ widgetTitle: title })
         }
       });
     }
@@ -105,6 +107,15 @@ class App extends React.Component {
               value={this.state.widgetId}
               onChange={({ target }) => this.setState({ widgetId: target.value })}
             />
+            <br />
+            <label htmlFor="title">Widget title (optional):</label>
+            <input
+              type="text"
+              placeholder="Title of the widget"
+              id="title"
+              value={this.state.widgetTitle}
+              onChange={({ target }) => this.setState({ widgetTitle: target.value })}
+            />
           </p>
         </div>
         <Icons />
@@ -113,10 +124,13 @@ class App extends React.Component {
         <WidgetEditor
           datasetId={this.state.datasetId}
           widgetId={this.state.widgetId}
+          widgetTitle={this.state.widgetTitle}
           saveButtonMode="always"
           embedButtonMode="always"
+          titleMode="always"
           onSave={() => this.onSave()}
           onEmbed={() => this.onEmbed()}
+          onChangeWidgetTitle={title => this.setState({ widgetTitle: title })}
           provideWidgetConfig={(func) => { this.getWidgetConfig = func; }}
         />
       </div>

commit 0b079422bea9796d2fec42a894c4597194ee9cbf

Fix a regression introduced by 03c70e7 (#4)
The title of a restored widget would be emptied right away.

src/components/WidgetEditor.js
@@ -219,9 +219,8 @@ class WidgetEditor extends React.Component {
     }
 
     // If the title is controlled from the outside and
-    // its value is changed, then we update the store
-    if (nextProps.widgetTitle !== undefined
-      && nextProps.widgetTitle !== this.props.widgetEditor.title) {
+    // its value has changed, then we update the store
+    if (this.props.widgetTitle !== nextProps.widgetTitle) {
       this.props.setTitle(nextProps.widgetTitle);
     }
   }
@@ -911,7 +910,12 @@ class WidgetEditor extends React.Component {
         if (limit) this.props.setLimit(limit);
         if (chartType) this.props.setChartType(chartType);
         if (areaIntersection) this.props.setAreaIntersection(areaIntersection);
-        if (name) this.props.setTitle(name);
+        if (name) {
+          this.props.setTitle(name);
+          if (this.props.onChangeWidgetTitle) {
+            this.props.onChangeWidgetTitle(name);
+          }
+        }
         if (zoom) this.props.setZoom(zoom);
         if (lat && lng) this.props.setLatLng({ lat, lng });
       });