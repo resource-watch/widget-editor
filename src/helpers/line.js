8dffd33aa3d45b3acc8063bae2255fee7c793e8c
Clément Prod'homme <clement.prodhomme@vizzuality.com>
2018-02-13 11:43:05 +0000

    Update the line chart to Vega 3

--- src/helpers/line.js
+++ src/helpers/line.js
@@ -7,44 +7,37 @@ import { getTimeFormat } from 'helpers/WidgetHelper';
 const defaultChart = {
   "$schema": "https://vega.github.io/schema/vega/v3.0.json",
   "data": [
-    {
-      "name": "table",
-      
-    }
+    { "name": "table" }
   ],
   "scales": [
     {
       "name": "x",
-      "type": "point",
+      "type": "linear",
       "range": "width",
       "nice": true,
-      "domain": {
-        "data": "table",
-        "field": "x"
-      }
+      "zero": false,
+      "domain": { "data": "table", "field": "x" }
     },
     {
       "name": "y",
       "type": "linear",
       "range": "height",
       "nice": true,
-      "zero": true,
-      "domain": {
-        "data": "table",
-        "field": "y"
-      }
+      "zero": false,
+      "domain": { "data": "table", "field": "y" }
     }
   ],
   "axes": [
     {
       "orient": "bottom",
       "scale": "x",
-      "labelOverlap":"parity",
-      "encode": {"labels": {
-        "enter": {
-          "align": {"value": "left"},
-          "baseline": {"value": "middle"}
-          
+      "labelOverlap": "parity",
+      "interpolate": { "value": "linear" },
+      "encode": {
+        "labels": {
+          "enter": {
+            "align": { "value": "left" },
+            "baseline": { "value": "middle"}
           }
         }
       }
@@ -52,62 +45,33 @@ const defaultChart = {
     {
       "orient": "left",
       "scale": "y",
-      "labelOverlap":"parity",
+      "labelOverlap": "parity",
       "encode": {}
     }
   ],
   "marks": [
-    { "name":"lines",
-    "interactive":false,
+    {
+      "name": "lines",
+      "interactive": false,
       "type": "line",
       "from": {
         "data": "table"
       },
-      
       "encode": {
         "enter": {
-          "defined":{"value":true},
-          "strokeCap":{"value":"round"},
-          "x": {
-            "scale": "x",
-            "field": "x"
-          },
-          "y": {
-            "scale": "y",
-            "field": "y"
-          },
-          "stroke": {
-            "value": "steelblue"
-          },
-          "strokeWidth": {
-            "value": 2
-          }
-        },
-        "update": {
-          "fillOpacity": {
-            "value": 1
-          }
-        },
-        "hover": {
-          "fillOpacity": {
-            "value": 0.5
-          }
+          "x": { "scale": "x", "field": "x" },
+          "y": { "scale": "y", "field": "y" },
+          "strokeWidth": { "value": 2 },
+          "strokeCap": { "value": "round" }
         }
       }
     },
     {
       "type": "path",
-      "from": {
-        "data": "lines"
-      },
+      "from": { "data": "lines" },
       "encode": {
         "enter": {
-          "stroke": {
-            "value": "transparent"
-          },
-          "fill": {
-            "value": "transparent"
-          }
+          "opacity": { "value": 0 }
         }
       },
       "transform": [
@@ -116,18 +80,14 @@ const defaultChart = {
           "x": "datum.x",
           "y": "datum.y",
           "size": [
-            {
-              "signal": "width"
-            },
-            {
-              "signal": "height"
-            }
+            { "signal": "width" },
+            { "signal": "height" }
           ]
         }
       ]
     }
   ],
-  interaction_config: [
+  "interaction_config": [
     {
       "name": "tooltip",
       "config": {
@@ -168,59 +128,52 @@ export default function ({ columns, data, url, embedData }) {
       "property": "data"
     };
   }
-//
-//  // We save the name of the columns for the tooltip
-//  {
-//    const xField = config.interaction_config[0].config.fields[1];
-//    const yField = config.interaction_config[0].config.fields[0];
-//    xField.label = columns.x.alias || columns.x.name;
-//    yField.label = columns.y.alias || columns.y.name;
-//  }
-//
-//  // If the x column is a date, we need to use a
-//  // temporal x scale and parse the x column as a date
-//  if (columns.x.type === 'date') {
-//    // We update the scale
-//    const xScale = config.scales.find(scale => scale.name === 'x');
-//    xScale.type = 'utc';
-//
-//    // We parse the x column as a date
-//    if (!config.data[0].format) config.data[0].format = {};
-//    config.data[0].format.parse = { x: 'date' };
-//
-//    // We make the axis use temporal ticks
-//    const xAxis = config.axes.find(a => a.type === 'x');
-//    xAxis.formatType = 'time';
-//
-//    // We compute an optimal format for the ticks
-//    const temporalData = data.map(d => d.x);
-//    const format = getTimeFormat(temporalData);
-//    if (format) xAxis.format = format;
-//
-//    // We also set the format for the tooltip
-//    config.interaction_config[0].config.fields[1].format = format;
-//  } else if (columns.x.type === 'number') {
-//    const allIntegers = data.length && data.every(d => parseInt(d.x, 10) === d.x);
-//    if (allIntegers) {
-//      const xField = config.interaction_config[0].config.fields[1];
-//      xField.format = '';
-//    }
-//  }
-//
-//  // If all the "dots" have the exact same y position,
-//  // the chart won't have any height
-//  // To fix that, we force the domain of the y scale
-//  // to be around the value
-//  const oneYValue = data.length && data.every(d => d.y === data[0].y);
-//  if (data.length === 1 || oneYValue) {
-//    const yScale = config.scales.find(scale => scale.name === 'y');
-//
-//    // The step is 20% of the value
-//    const step = data[0].y * 0.2;
-//
-//    // We fix the domain around the value
-//    yScale.domain = [data[0].y - step, data[0].y + step];
-// }
+
+  // We save the name of the columns for the tooltip
+  {
+    const xField = config.interaction_config[0].config.fields[1];
+    const yField = config.interaction_config[0].config.fields[0];
+    xField.label = columns.x.alias || columns.x.name;
+    yField.label = columns.y.alias || columns.y.name;
+  }
+
+  // If the x column is a date, we need to use a
+  // temporal x scale and parse the x column as a date
+  if (columns.x.type === 'date') {
+    // We update the scale
+    const xScale = config.scales.find(scale => scale.name === 'x');
+    xScale.type = 'utc';
+
+    // We parse the x column as a date
+    if (!config.data[0].format) config.data[0].format = {};
+    config.data[0].format.parse = { x: 'date' };
+
+    // We compute an optimal format for the tooltip
+    const temporalData = data.map(d => d.x);
+    const format = getTimeFormat(temporalData);
+    config.interaction_config[0].config.fields[1].format = format;
+  } else if (columns.x.type === 'number') {
+    const allIntegers = data.length && data.every(d => parseInt(d.x, 10) === d.x);
+    if (allIntegers) {
+      const xField = config.interaction_config[0].config.fields[1];
+      xField.format = '';
+    }
+  }
+
+  // If all the "dots" have the exact same y position,
+  // the chart won't have any height
+  // To fix that, we force the domain of the y scale
+  // to be around the value
+  const oneYValue = data.length && data.every(d => d.y === data[0].y);
+  if (data.length === 1 || oneYValue) {
+    const yScale = config.scales.find(scale => scale.name === 'y');
+
+    // The step is 20% of the value
+    const step = data[0].y * 0.2;
+
+    // We fix the domain around the value
+    yScale.domain = [data[0].y - step, data[0].y + step];
+  }
 
   return config;
 };
